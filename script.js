cd045bba347e3563196ee3ae3606da2329047e65
ofcourseitsgv <ygenji4@gmail.com>
2025-06-01 19:46:41 -0700

    more style changes, add rusa

--- script.js
+++ script.js
@@ -61,3 +61,23 @@ const scrollUp = () => {
 }
 
 document.addEventListener('scroll', scrollUp)
+
+var slideIndex = [1,1];
+var slideId = ["slides1", "slides2"]
+showDivs(1, 0);
+showDivs(1, 1);
+
+function plusDivs(n, no) {
+  showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
